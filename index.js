a18a3fc5be56914d455c61ecbe1e13a2e0f77292
Muhammad <muh.hussien7@gmail.com>
2022-07-28 05:04:38 +0200

    adjustment 2

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const btns = document.querySelectorAll('.btn');
-const storeProducts = document.querySelectorAll('.store-product');
+const btns = document.querySelectorAll('.market_btn');
+const market_item = document.querySelectorAll('.market_item');
 // const search = document.getElementById(search);
 
 for (i = 0; i < btns.length; i++) {
@@ -10,7 +10,7 @@ for (i = 0; i < btns.length; i++) {
         const filter = e.target.dataset.filter;
         console.log(filter);
         
-        storeProducts.forEach((product)=> {
+        market_item.forEach((product)=> {
             if (filter === 'all'){
                 product.style.display = 'block'
             } else {
